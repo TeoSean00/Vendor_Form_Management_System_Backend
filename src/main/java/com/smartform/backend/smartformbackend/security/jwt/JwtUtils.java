81a3fe21ebb4bc333a511f8a75ecf738c3a8fd23
JosephJL <HeeJLJoseph@gmail.com>
2023-02-26 16:57:29 +0800

    updated keyname

--- src/main/java/com/smartform/backend/smartformbackend/security/jwt/JwtUtils.java
+++ src/main/java/com/smartform/backend/smartformbackend/security/jwt/JwtUtils.java
@@ -15,10 +15,10 @@ import io.jsonwebtoken.*;
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${bezkoder.app.jwtSecret}")
+    @Value("${smartform.app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${bezkoder.app.jwtExpirationMs}")
+    @Value("${smartform.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
