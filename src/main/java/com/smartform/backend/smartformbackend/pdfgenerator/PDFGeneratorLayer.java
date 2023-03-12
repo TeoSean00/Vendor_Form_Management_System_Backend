3e3d3fd111d252398c3a6927cd30e22d00883217
Xun Yi <xunyi.work@gmail.com>
2023-03-12 14:28:20 +0800

    Added in json to pdf, not tested yet

---
+++ src/main/java/com/smartform/backend/smartformbackend/pdfgenerator/PDFGeneratorLayer.java
@@ -0,0 +1,40 @@
+package com.smartform.backend.smartformbackend.pdfgenerator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PDFGeneratorLayer {
+    public static void main(String[] args) {
+        String filePath = "generated/form.docx"; // Preset name
+        JsonToWord json2word = new JsonToWord();
+        json2word.createDocument(filePath);
+        //Expect to take in a big json object
+        ArrayList<HashMap<String,Object>> inputList = null;
+        for (HashMap<String,Object> input: inputList ){
+            //Checks object type 
+            //Currently missing header section, signature, radio group 
+            if (input.get("type") == "header"){
+                json2word.createHeader(input);
+            }
+            else if (
+                input.get("type") == "text" ||
+                input.get("type") == "date" ||
+                input.get("type") == "number" 
+            ) {
+                json2word.createTextInput(input);
+            }
+            else if (input.get("type") == "boolean") {
+                json2word.createBoolean(input);
+            }
+            else if (input.get("type") == "checkbox") {
+                json2word.createRadioGroup(input);
+            }
+            else if (input.get("type") == "likertGroup") {
+                json2word.createLikertGroup(input);
+            }
+        }
+        json2word.saveDocument();
+        json2word.saveToPdf(filePath);
+    }
+    
+}
