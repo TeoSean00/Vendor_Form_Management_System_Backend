c45cdaa5d60253aef03be7131a6c09a674112530
JosephJL <HeeJLJoseph@gmail.com>
2023-03-20 16:32:01 +0800

    Update VendorController

--- src/main/java/com/smartform/backend/smartformbackend/vendor/VendorDAO.java
+++ src/main/java/com/smartform/backend/smartformbackend/vendor/VendorDAO.java
@@ -1,6 +1,14 @@
 package com.smartform.backend.smartformbackend.vendor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -39,5 +47,48 @@ public class VendorDAO {
 
     public Vendor getVendor(final String topicId) {
         return mongoTemplate.findById(topicId, Vendor.class);
+    }
+
+    public Map<Integer, JoinDateDTO> getJoinDates() {
+        List<Vendor> vendors = mongoTemplate.findAll(Vendor.class);
+        List<String> months = Arrays.asList("January", "Febuary", "March", "April", "May", "June", "July",
+                "August", "September", "October", "November", "December");
+        Map<Integer, Map<String, Integer>> joinDates = new HashMap<>();
+        for (Vendor vendor : vendors) {
+            // Date joinDate = vendor.getJoinDate();
+            // JoinDateDTO joinDateDTO = new JoinDateDTO();
+            // Calendar calendar = Calendar.getInstance();
+            // calendar.get(Calendar.YEAR);
+
+            // int mthIdx = calendar.get(Calendar.MONTH);
+            // String monthName = months.get(mthIdx);
+            // int monthDay = calendar.get(Calendar.DAY_OF_MONTH);
+            // joinDateDTO.getMonths().put(monthName, monthDay);
+            // joinDates.add(joinDateDTO);
+            Date joinDate = vendor.getJoinDate();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(joinDate);
+            int year = calendar.get(Calendar.YEAR);
+            int monthDay = calendar.get(Calendar.MONTH);
+            String monthName = months.get(monthDay);
+
+            Map<String, Integer> yearMonthMap = joinDates.getOrDefault(year, new HashMap<>());
+            yearMonthMap.merge(monthName, 1, Integer::sum);
+            joinDates.put(year, yearMonthMap);
+
+        }
+        Map<Integer, JoinDateDTO> result = new HashMap<>();
+        for (Map.Entry<Integer, Map<String, Integer>> entry : joinDates.entrySet()) {
+            JoinDateDTO joinDateDTO = new JoinDateDTO();
+            Map<String, Integer> currYearMonthMap = entry.getValue();
+            for (Map.Entry<String, Integer> monthEntry : currYearMonthMap.entrySet()) {
+                String Name = monthEntry.getKey();
+                int count = monthEntry.getValue();
+                joinDateDTO.getMonths().put(Name, count);
+            }
+            result.put(entry.getKey(), joinDateDTO);
+        }
+        return result;
+
     }
 }
